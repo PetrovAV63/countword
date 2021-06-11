66d72659678593e408a21cf25f189885d97b5db9
Enai63 <Toha76@yandex.ru>
2021-06-11 22:08:42 +0400

    add TODO response code

--- src/main/java/ru/enai/countword/service/SaveHtml.java
+++ src/main/java/ru/enai/countword/service/SaveHtml.java
@@ -2,18 +2,14 @@ package ru.enai.countword.service;
 
 
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.enai.countword.service.interfaces.SaveServiceHtml;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,23 +26,37 @@ public class SaveHtml implements SaveServiceHtml {
     public String saveHtmlInFile(String link) {
         String fileName = link.substring(link.indexOf('.') + 1, link.lastIndexOf('.'));
         String pathFileHtml = "";
-        Path directories;
         Path newFilePath;
         try {
-            directories = Files.createDirectories(Path.of(path));
-            newFilePath = Paths.get(directories + "/" + fileName + ".html");
-            Set<String> singleton;
+            newFilePath = getPath(fileName);
+
             String document = null;
-            if (!Files.exists(newFilePath)) {
-                Files.createFile(newFilePath);
+            Set<String> singleton;
+
+            Connection.Response response = Jsoup.connect(link).execute();
+
+            if (response.statusCode() != 200) {
+                //TODO check response code
             }
             pathFileHtml = newFilePath.toAbsolutePath().toString();
             document = Jsoup.connect(link).get().outerHtml();
             singleton = Collections.singleton(document);
             Files.write(newFilePath, singleton);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return pathFileHtml;
+    }
+
+    private Path getPath(String fileName) throws IOException {
+        Path newFilePath;
+        Path directories;
+        directories = Files.createDirectories(Path.of(path));
+        newFilePath = Paths.get(directories + "/" + fileName + ".html");
+        if (!Files.exists(newFilePath)) {
+            Files.createFile(newFilePath);
+        }
+        return newFilePath;
     }
 }
